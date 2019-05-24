058406b3e83fb7d1e3afc496a00344e8335ab25f
paulantoine2 <paul.antoine2@gmail.com>
2019-05-24 23:03:52 +0900

    V2

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FlamelinkService } from '../../flamelink.service';
+import {Experience} from './experience.model';
 
 @Component({
   selector: 'experiences',
@@ -8,13 +9,15 @@ import { FlamelinkService } from '../../flamelink.service';
 })
 export class ExperienceComponent implements OnInit {
   steps = [];
-  active = 1;
+  active = 0;
 
   constructor(private fl: FlamelinkService) { }
 
   ngOnInit() {
     this.fl.getAllContent('experience').subscribe(
-      (data) => this.steps = Object.values(data)
+      (data) => {
+        this.steps = Object.values(data).sort((a: Experience, b: Experience) => (a.datePeriod < b.datePeriod) ? 1 : -1);
+      }
     );
   }
 
---
+++ src/app/components/experience/experience.model.ts
@@ -0,0 +1,7 @@
+export interface Experience {
+  datePeriod: string;
+  location: string;
+  company: string;
+  title: string;
+  description: string;
+}
